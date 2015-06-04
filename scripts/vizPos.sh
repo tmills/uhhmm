91b032b3bb20d39483d151ee881fad99aeb03b64
tmill <tim.miller@gmail.com>
2015-06-04 08:57:57 -0400

    Made viz script more useful.

--- scripts/vizPos.sh
+++ scripts/vizPos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for i in `seq 1 $2`; do
+for i in `seq 0 $2`; do
   echo "********** POS $i *************"
-  grep -v 000000 $1 | grep " $i " | sort -nrk7 | head
+  grep -v 000000 $1 | grep " $i " | sort -nrk7 | head -20
 done
 
