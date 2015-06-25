ed91d42aee6f13376ee2d1ca50610a2c59cbc135
tmill <tim.miller@gmail.com>
2015-06-25 06:34:07 -0400

    Changes to sampler to allow new changes in ihmm to work.

--- scripts/ihmm_sampler.py
+++ scripts/ihmm_sampler.py
@@ -5,29 +5,49 @@ import logging
 
 ## TODO -- instead of being passed H have the Model class implement
 ## a get_base_distribution() method that can be called
-def sampleDirichlet(counts, H, nullState = False):
+def sampleDirichlet(counts, H):
     L = max(H.shape)
     K = counts.pairCounts.shape[0]
     P = np.zeros(counts.pairCounts.shape)
     
-#    logging.debug("Shape of P is %s", P.shape)
-    
     ## Sample from a dirichlet to get distribution over
     ## words for every pos tag:
     for i in range(0,K):
-        base = counts.pairCounts[i,:]+H
-#        logging.debug("i is %d", i)
-#        logging.debug("Shape of pairCounts slice is %s", counts.pairCounts[i,:].shape)
-#        logging.debug("Shape of H is %s", H.shape)
-#        logging.debug("Shape of base is %s", base.shape)
-#        logging.debug("Shape of P is %s", P.shape)
-#        logging.debug("Shape of P[i,:] is %s", P[i,:].shape)
-#        logging.debug(base)
-        ## base[0][0] will be 0 but needs to be positive for gamma
-        if nullState:
-            base[0][0] = 1
-        P[i,:] = np.random.gamma(base,1)
-        if nullState:
-            P[i,0] = 0
-        P[i,:] /= sum(P[i,:])
-    return np.log10(P)
+        ## Set last value (new table probability):
+        if H.ndim == 2:
+            hrow = H[0]
+        elif H.ndim == 1:
+            hrow = H
+
+        P[i,-1] = hrow[-1]
+                
+        ## Get distribution for existing table by slicing out the null state
+        ## and the new table state:
+        base = np.zeros((1,L-2)) + counts.pairCounts[i,1:-1]+hrow[1:-1]
+        
+        if base.shape[1] == 1:
+            P[i,1] = 1.0
+        else:
+            ## Get the distribution normalized relative to the new table prob:
+            dist = np.random.gamma(base, 1)
+            dist /= dist.sum()
+            dist *= (1 - P[i,-1])
+            P[i,0] = -np.inf
+            P[i,1:-1] = np.log10(dist)
+            P[i,-1] = np.log10(P[i,-1])
+
+    return P
+
+def sampleBernoulli(counts, H):
+    ## How many inputs?
+    K = counts.pairCounts.shape[0]
+    assert counts.pairCounts.shape[1] == 2, 'This method can only be called for a distribution with 2 outputs (F or J models)'
+    
+    P = np.zeros((K,2))
+    
+    for i in range(0,K):
+        base = counts.pairCounts[i,:] + H.flatten()
+        P[i,:] = np.log10(np.random.dirichlet(base, 1)[0])
+        
+    return P
+        
