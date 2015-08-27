ce4d952a07fa57f619d3a08679da00fd46396587
Tim Miller <timothy.miller@childrens.harvard.edu>
2015-08-27 15:00:26 -0400

    Dramatically increased model compilation time with some caching and better ordering.

--- scripts/finite_sampler.pyx
+++ scripts/finite_sampler.pyx
@@ -25,77 +25,88 @@ def compile_models(totalK, models):
 #    phi = np.zeros((totalK, models.lex.dist.shape[1]))
     pi = np.zeros((totalK, totalK))
     phi = np.zeros((totalK, models.lex.dist.shape[1]))
-    
+    cache = np.zeros((a_max, b_max, g_max, totalK)) - 1
+        
     ## Take exponent out of inner loop:
     word_dist = 10**models.lex.dist
-
+    
     (fork,act,root,cont,start,pos) = unlog_models(models)
+
+    ## Build the observation matrix first:
+    for state in range(0,totalK):
+        (f,j,a,b,g) = ihmm.extractStates(state, totalK)
+        phi[state] = word_dist[g,:]
 
     ## For previous state i:
     for prevState in range(0,totalK):
         (prevF, prevJ, prevA, prevB, prevG) = ihmm.extractStates(prevState, totalK)
         cumProbs = np.zeros(5)
         
-        prevBG = ihmm.bg_state(prevB,prevG)
-        ## Sample f & j:
-        for f in (0,1):
-            if prevA == 0 and prevB == 0 and f == 0:
-                continue
-
-            cumProbs[0] = (fork[prevBG,f])
-
-            for j in (0,1):
-                ## At depth 1 -- no probability model for j
-                if prevA == 0 and prevB == 0:
-                    ## index 1:
-                    if j == 0:
+        if cache[prevA][prevB][prevG].sum() > 0:
+            pi[prevState] = cache[prevA][prevB][prevG]
+        
+        else:
+            prevBG = ihmm.bg_state(prevB,prevG)
+            ## Sample f & j:
+            for f in (0,1):
+                if prevA == 0 and prevB == 0 and f == 0:
+                    continue
+    
+                cumProbs[0] = (fork[prevBG,f])
+    
+                for j in (0,1):
+                    ## At depth 1 -- no probability model for j
+                    if prevA == 0 and prevB == 0:
+                        ## index 1:
+                        if j == 0:
+                            cumProbs[1] = cumProbs[0]
+                        else:
+                            cumProbs[1] = 0
+                            ## No point in continuing -- matrix is zero'd to start
+                            continue
+                
+                    elif f == j:
                         cumProbs[1] = cumProbs[0]
                     else:
                         cumProbs[1] = 0
-                        ## No point in continuing -- matrix is zero'd to start
-                        continue
-                
-                elif f == j:
-                    cumProbs[1] = cumProbs[0]
-                else:
-                    cumProbs[1] = 0
-                    continue    
+                        continue    
         
-                for a in range(1,a_max):
-                    if f == 0 and j == 0:
-                        ## active transition:
-                        cumProbs[2] = cumProbs[1] * (act[prevA,a])
-                    elif f == 1 and j == 0:
-                        ## root -- technically depends on prevA and prevG
-                        ## but in depth 1 this case only comes up at start
-                        ## of sentence and prevA will always be 0
-                        cumProbs[2] = cumProbs[1] * (root[prevG,a])
-                    elif f == 1 and j == 1 and prevA == a:
-                        cumProbs[2] = cumProbs[1]
-                    else:
-                        ## zero probability here
-                        continue
-        
-                    if cumProbs[2] == 0:
-                        continue
-
-                    prevAa = ihmm.aa_state(prevA, a)
-        
-                    for b in range(1,b_max):
-                        if j == 1:
-                            cumProbs[3] = cumProbs[2] * (cont[prevBG,b])
+                    for a in range(1,a_max):
+                        if f == 0 and j == 0:
+                            ## active transition:
+                            cumProbs[2] = cumProbs[1] * (act[prevA,a])
+                        elif f == 1 and j == 0:
+                            ## root -- technically depends on prevA and prevG
+                            ## but in depth 1 this case only comes up at start
+                            ## of sentence and prevA will always be 0
+                            cumProbs[2] = cumProbs[1] * (root[prevG,a])
+                        elif f == 1 and j == 1 and prevA == a:
+                            cumProbs[2] = cumProbs[1]
                         else:
-                            cumProbs[3] = cumProbs[2] * (start[prevAa,b])
+                            ## zero probability here
+                            continue
+        
+                        if cumProbs[2] == 0:
+                            continue
+
+                        prevAa = ihmm.aa_state(prevA, a)
+        
+                        for b in range(1,b_max):
+                            if j == 1:
+                                cumProbs[3] = cumProbs[2] * (cont[prevBG,b])
+                            else:
+                                cumProbs[3] = cumProbs[2] * (start[prevAa,b])
             
-                        # Multiply all the g's in one pass:
-                        ## range gets the range of indices in the forward pass
-                        ## that are contiguous in the state space
-                        state_range = ihmm.getStateRange(f,j,a,b)
+                            # Multiply all the g's in one pass:
+                            ## range gets the range of indices in the forward pass
+                            ## that are contiguous in the state space
+                            state_range = ihmm.getStateRange(f,j,a,b)
                                      
-                        range_probs = cumProbs[3] * (pos[b,:])
-                        pi[prevState, state_range] = range_probs
-                        phi[state_range,:] = word_dist
-    
+                            range_probs = cumProbs[3] * (pos[b,:])
+                            pi[prevState, state_range] = range_probs
+
+            cache[prevA][prevB][prevG] = pi[prevState,:]
+                            
     time_spent = time.time() - t0
     logging.info("Done in %d s" % time_spent)
 #    return (np.matrix(pi, copy=False), np.matrix(phi, copy=False))
