3cb3967e3deaf21d0f8ca6e87e973d9c929790da
Bill Bryce <bryce2@illinois.edu>
2015-09-18 16:59:00 -0500

    added pyzmq code with dummy workload

---
+++ scripts/pyzmq_sink.py
@@ -0,0 +1,37 @@
+# Task sink
+# Binds PULL socket to tcp://localhost:5558
+# Collects results from workers via that socket
+#
+# Author: Lev Givon <lev(at)columbia(dot)edu>
+# Modified by Bill Bryce <bryce2(at)illinois(dot)edu>
+
+import sys
+import time
+import zmq
+
+
+context = zmq.Context()
+
+# Socket to receive messages on
+receiver = context.socket(zmq.PULL)
+receiver.bind("tcp://*:5558")
+
+# Wait for start of batch
+s = receiver.recv()
+
+# Start our clock now
+tstart = time.time()
+
+# Process 100 confirmations
+total_msec = 0
+for task_nbr in range(100):
+    s = receiver.recv()
+    if task_nbr % 10 == 0:
+        sys.stdout.write(':'+s+':')
+    else:
+        sys.stdout.write(s)
+    sys.stdout.flush()
+
+# Calculate and report duration of batch
+tend = time.time()
+print("Total elapsed time: %d msec" % ((tend-tstart)*1000))
---
+++ scripts/pyzmq_work.py
@@ -0,0 +1,37 @@
+# Task worker
+# Connects PULL socket to tcp://localhost:5557
+# Collects workloads from ventilator via that socket
+# Connects PUSH socket to tcp://localhost:5558
+# Sends results to sink via that socket
+#
+# Author: Lev Givon <lev(at)columbia(dot)edu>
+# Modified by Bill Bryce <bryce2(at)illinois(dot)edu>
+
+import sys
+import time
+import zmq
+
+
+context = zmq.Context()
+
+# Socket to receive messages on
+receiver = context.socket(zmq.PULL)
+receiver.connect("tcp://localhost:5557")
+
+# Socket to send messages to
+sender = context.socket(zmq.PUSH)
+sender.connect("tcp://localhost:5558")
+
+# Process tasks forever
+while True:
+    s = receiver.recv()
+
+    # Simple progress indicator for the viewer
+    sys.stdout.write('(1)')
+    sys.stdout.flush()
+
+    # Do the work
+    time.sleep(int(s)*0.001)
+
+    # Send results to sink
+    sender.send(b'(Q1)')
