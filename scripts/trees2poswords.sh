bf16fbd11b8355ad044905f7400a9afb13479aa4
Tim Miller <timothy.miller@childrens.harvard.edu>
2015-10-08 16:49:38 -0400

    Fixed bug that gets rid of traces in wsj input.

--- scripts/trees2poswords.sh
+++ scripts/trees2poswords.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-perl -pe 's/\(([^ -]+)\S* ([^\s\)\(]+)\)/[\1\/\2]/g;s/\(\S*//g;s/\)//g;s/^\s*//g;s/  +/ /g;s/[\[\]]//g'
+## REGEXP 1: (PHRASE (POS word)) -> (PHRASE [POS/word])  (find terminals and rewrite with brackets)
+## REGEXP 2: (PHRASE ->                                  (remove phrase nodes)
+## REGEXP 3: ) ->                                        (remove right parens and any traces that are with them)
+## REGEXP 4: [pos/word]  [pos/word] -> [pos/word] [pos/word] (remove extra whitespace)
+## REGEXP 5: [pos/word] -> pos/word                          (remove square brackets)
+
+perl -pe 's/\(([^ -]+)\S* ([^\s\)\(]+)\)/[\1\/\2]/g;s/\(\S*//g;s/[^\[\]\s]*\)//g;s/^\s*//g;s/  +/ /g;s/[\[\]]//g;'
 
