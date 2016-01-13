04b751feef47f8c745d4f5e94ce4ceae93acd65d
tmill <tim.miller@gmail.com>
2016-01-12 20:35:12 -0500

    Fix to script that extracts tagword input from conll dependency parses.

--- scripts/conll2words.pl
+++ scripts/conll2words.pl
@@ -10,10 +10,21 @@ while(<STDIN>){
 #    print "End sentence of length".$#sent."\n";
     print join(" ", @sent)."\n";
     @sent = ();
-  }else{
+  }elsif(m/^\d+\s/){
     my @fields = split /\s+/;
-    my $tagword = $fields[6]."/".$fields[1];
-    push @sent, $tagword;
+    if($fields[2] =~ m/\+/){
+        ## Iterate over morpho tokens
+        my @words = split /\+/, $fields[2];
+        my @tags = split /\+/, $fields[3];
+        for my $i (0..$#words){
+            my $tagword = $tags[$i]."/".$words[$i];
+            push @sent, $tagword;
+        }
+    }else{    
+        ## Just one morpho token -- push it onto sentence
+        my $tagword = $fields[3]."/".$fields[1];
+        push @sent, $tagword;
+    }
   }
 }
 
