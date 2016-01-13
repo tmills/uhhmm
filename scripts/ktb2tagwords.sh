f78ba33894bfeaa1348078b436de51fee0632361
tmill <tim.miller@gmail.com>
2016-01-13 09:25:53 -0500

    Script for converting penn korean treebank into format we can read.

---
+++ scripts/ktb2tagwords.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+for i in $1/*.fid; do
+#    echo $i
+    cat $i | grep -v "^;;" | perl -pe 's/^(.*)/<SOL>\1/g;s/\n/ /g' | perl -pe 's/<SOL> <SOL>/\n/g' | perl -pe 's/<SOL>//g' | perl -pe 's/\(\S+//g;s/\)//g;s/  */ /g' | perl -pe 's/\+/ /g' | perl -pe 's/(\S+)\/(\S+)/\2\/\1/g' | perl -pe 's/ \*\S*//g' | perl -pe 's/^ *//g' | grep -v "^$"
+done
