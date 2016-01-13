77bb98bda111414f1bb09733524f9badc430b4cb
Tim Miller <timothy.miller@childrens.harvard.edu>
2016-01-13 09:34:15 -0500

    Some fixes to korean treebank script for encoding and line endings.

--- scripts/ktb2tagwords.sh
+++ scripts/ktb2tagwords.sh
@@ -3,5 +3,5 @@
 
 for i in $1/*.fid; do
 #    echo $i
-    cat $i | grep -v "^;;" | perl -pe 's/^(.*)/<SOL>\1/g;s/\n/ /g' | perl -pe 's/<SOL> <SOL>/\n/g' | perl -pe 's/<SOL>//g' | perl -pe 's/\(\S+//g;s/\)//g;s/  */ /g' | perl -pe 's/\+/ /g' | perl -pe 's/(\S+)\/(\S+)/\2\/\1/g' | perl -pe 's/ \*\S*//g' | perl -pe 's/^ *//g' | grep -v "^$"
+    cat $i | fromdos | iconv -f EUC-KR -t UTF-8 | grep -v "^;;" | perl -pe 's/^(.*)/<SOL>\1/g;s/\n/ /g' | perl -pe 's/<SOL> <SOL>/\n/g' | perl -pe 's/<SOL>//g' | perl -pe 's/\(\S+//g;s/\)//g;s/  */ /g' | perl -pe 's/\+/ /g' | perl -pe 's/(\S+)\/(\S+)/\2\/\1/g' | perl -pe 's/ \*\S*//g' | perl -pe 's/^ *//g' | grep -v "^$"
 done
