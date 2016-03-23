90f76285437ff1085927459bc88bd4d56f50288e
tmill <tim.miller@gmail.com>
2016-03-23 18:51:58 -0400

    added numpy include to setup file

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 from distutils.extension import Extension
-
+import numpy
 
 extensions = [Extension("*", ["scripts/*.pyx"], 
                 )]
 
 setup(
-    ext_modules = cythonize(extensions)
+    ext_modules = cythonize(extensions),
+    include_dirs=[numpy.get_include()]
 )
