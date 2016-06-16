e2f42060fdb9432c669567a56f687b979bb66f32
tmill <tim.miller@gmail.com>
2016-06-16 07:36:04 -0400

    WIP - first pass with all code commented out.

--- scripts/FullDepthCompiler.pyx
+++ scripts/FullDepthCompiler.pyx
@@ -18,6 +18,151 @@ from PyzmqMessage import ModelWrapper
 import Indexer
 import scipy.sparse
 
+# def compile_one_column(int depth, int next_index, models, indexer):
+#     cdef int totalK, a_max, b_max, g_max, start_depth, above_act, above_awa, state_index
+#     cdef int f,j,a,b,g, prevF, prefJ
+#     cdef np.ndarray range_probs, prevA, prevB
+# 
+#     next_state = indexer.extractState(next_index)
+#     (nextF, nextJ, nextA, nextB, nextG) = next_state.f, next_state.j, next_state.a, next_state.b, next_state.g
+#     next_depth = get_cur_awa_depth(nextB)
+# 
+#     (a_max, b_max, g_max) = indexer.getVariableMaxes()
+#     totalK = indexer.get_state_size()        
+#     indices =  []
+#     data = []
+# 
+#     ## Skip invalid end states:
+#     if depth > 1:
+#         ## Others that are allowed but not really possible:
+#         ## 1) where lower depths have real values but higher depths have
+#         ## placeholder 0 values
+#         ## 2) where one of A and B has a placeholder at the same depth level
+#         lowest_depth = -1
+#         for d in range(depth-1, -1, -1):
+#             if(nextA[d] * nextB[d] == 0 and nextA[d] + nextB[d] != 0):
+#                 ## Exactly one of the values at this depth is zero -- not allowed
+#                 return indices, data
+#             if lowest_depth == -1:
+#                 if nextA[d] > 0:
+#                     ## Until we find a non-zero value move the depth up
+#                     lowest_depth = d
+#             else:
+#                 ## we have seen a non-zero value deeper than us, so we can't see any zeros now
+#                 if nextA[d] == 0 or nextB[d] == 0:
+#                     return indices, data
+#     
+#     cumProbs = np.zeros(3)
+#     prev_state = State(depth)
+# 
+#     t00 = time.time()
+#     for f in (0,1):
+#         for j in (0, 1):
+#             for a in range(0, a_max-1):
+#                 for b in range(0, b_max-1):
+#                     ## a can be 0 at t=0 but b must also be 0
+#                     if (a == 0 and b != 0) or (a != 0 and b == 0):
+#                         continue
+#                         
+#                     for g in range(1, g_max-1):
+#                         if a == 0 and b == 0:
+#                             cumProbs[0] = cumProbs[1] = 1.0
+#                         else:
+#                             cumProbs[0] = models.fork[start_depth].dist[ b, g, nextF ]
+#                         
+#                             if nextF == 0:
+#                                 cumProbs[1] = cumProbs[0] * models.reduce[start_depth].dist[ a, above_awa, nextJ ]
+#                             
+#                     
+#         prev_state.f = f
+# 
+#         ## when t=0, start_depth will be -1, which in the d> 1 case will wraparound.
+#         ## we want in the t=0 case for f_{t=1} to be [-/-]*d
+#         if start_depth >= 0:
+#             cumProbs[0] = models.fork[start_depth].dist[ prevB[start_depth], prevG, f ]
+#         else:
+#             ## if start depth is -1 we're only allowed to fork:
+#             if f == 1:
+#                 cumProbs[0] = 1.0
+#             else:
+#                 continue
+#             
+#         for j in (0,1):
+#             nextState.j = j
+#             ## See note above where we set nextState.f
+#             if start_depth >= 0:
+#                 if f == 0:
+#                     cumProbs[1] = cumProbs[0] * models.reduce[start_depth].dist[ prevA[start_depth], above_awa, j ]
+#                 else:
+#                     cumProbs[1] = cumProbs[0] * models.trans[start_depth].dist[ prevB[start_depth], prevG, j ]
+#             else:
+#                 if j == 0:
+#                     cumProbs[1] = 1
+#                 else:
+#                     ## if start_depth <0 then j must be 0
+#                     continue
+#             
+#             for a in range(1, a_max-1):
+#                 nextState.a[:] = 0
+#                 for b in range(1, b_max-1):
+#                     nextState.b[:] = 0
+# 
+#                     if f == 1 and j == 1:
+#                         if a == prevA[start_depth]:
+#                             nextState.a[0:start_depth] = prevA[0:start_depth]
+#                             nextState.b[0:start_depth] = prevB[0:start_depth]
+#                         
+#                             nextState.a[start_depth] = a
+#                         
+#                             cumProbs[2] = cumProbs[1] * models.cont[start_depth].dist[ prevB[start_depth], prevG, b]
+#                             nextState.b[start_depth] = b
+#                         
+#                         else:
+#                             continue
+#                     elif f == 0 and j == 1:
+#                         ## -/+ case, reducing a level unless already at minimum depth
+#                         if start_depth <= 0:
+#                             continue
+#                         nextState.a[0:start_depth-1] = prevA[0:start_depth-1]
+#                         nextState.b[0:start_depth-1] = prevB[0:start_depth-1]
+#                     
+#                         if a == prevA[start_depth-1]:
+# #                                cumProbs[2] = cumProbs[1]
+#                             nextState.a[start_depth-1] = a
+#                             nextState.b[start_depth-1] = b
+#                             cumProbs[2] = cumProbs[1] * models.next[start_depth-1].dist[ prevA[start_depth], above_awa, b ]
+#                         else:
+#                             continue
+#                     elif f == 0 and j == 0:
+#                         ## -/-, reduce in place
+#                         nextState.a[0:start_depth] = prevA[0:start_depth]
+#                         nextState.b[0:start_depth] = prevB[0:start_depth]
+#                         nextState.a[start_depth] = a
+#                         cumProbs[2] = cumProbs[1] * models.act[start_depth].dist[ prevA[start_depth], above_awa, a] * models.start[start_depth].dist[ prevA[start_depth], a, b ]
+#                     
+#                         nextState.b[start_depth] = b
+#                     elif f == 1 and j == 0:
+#                         ## +/-, create a new stack level unless we're at the limit
+#                         if start_depth+1 == depth:
+#                             continue
+#                 
+#                         nextState.a[0:start_depth+1] = prevA[0:start_depth+1]
+#                         nextState.b[0:start_depth+1] = prevB[0:start_depth+1]
+#                         nextState.a[start_depth+1] = a
+#                         cumProbs[2] = cumProbs[1] * models.root[start_depth+1].dist[ above_awa, prevG, a ] * models.exp[start_depth+1].dist[ prevG, a, b ]
+#                     
+#                         nextState.b[start_depth+1] = b
+#                                             
+#                     ## Now multiply in the pos tag probability:
+#                     state_index = indexer.getStateIndex(nextState.f, nextState.j, nextState.a, nextState.b, 0)         
+#                     range_probs = cumProbs[2] * (models.pos.dist[b,:-1])
+#                     #logging.info("Building model with %s => %s" % (prev_state.str(), nextState.str() ) )
+#                     for g in range(1,len(range_probs)):
+#                         indices.append(state_index + g)
+#                         data.append(range_probs[g])
+# 
+#     return indices, data
+
 #@profile
 def compile_one_line(int depth, int prevIndex, models, indexer):
     cdef int totalK, a_max, b_max, g_max, start_depth, above_act, above_awa, state_index
