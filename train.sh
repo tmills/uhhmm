13e8c40f9f60318bd36588b7f478fc1dd3a330a3
Tim Miller <timothy.miller@childrens.harvard.edu>
2016-09-26 17:30:18 -0400

    Made train call Make by default

--- train.sh
+++ train.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-./gpusrc/compile_linux.sh
+#./gpusrc/compile_linux.sh
 #./gpusrc/compile.sh
 #./gpusrc/compile_osc.sh
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+
+make scripts/CHmmSampler.so || exit
+
 python3 setup.py build_ext || exit
 if [ $# -ne 1 ]; 
     then echo "Running the script requires a config argument; exiting."
