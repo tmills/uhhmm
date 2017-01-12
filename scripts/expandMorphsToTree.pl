6c4f997d6ef7d05bf1ae5efb84cf5bd3e8bd819f
tmill <tim.miller@gmail.com>
2017-01-11 20:51:18 -0500

    Fix to morphs with spaces instead of plus signs. also documented script.

--- scripts/expandMorphsToTree.pl
+++ scripts/expandMorphsToTree.pl
@@ -1,5 +1,8 @@
 #!/usr/bin/perl
-
+#
+# This script converts the PTB tres with morphological segmentation like:
+# (NP token1/NN token2/NN token3/PAN) => (NP (NN token1) (NN token2) (PAN token3))
+#
 use strict;
 
 while(<STDIN>){
@@ -7,23 +10,17 @@ while(<STDIN>){
     my @matches = m/\((\S+) ([^\)\(]+)\)/g;
     
     for(my $i = 0; $i < $#matches; $i = $i + 2){
-#      print "### New match ###\n";
-#      print $matches[$i] . " " . $matches[$i+1] . "\n";
       my $cat = $matches[$i];
       my $lex = $matches[$i+1];
       
-      my @morphs = split /\+/, $lex;
-#      print "Morphs are @morphs\n";
+      my @morphs = split /[\+ ]/, $lex;
       if($#morphs > 0){
         my $expanded_lex = "";
         for my $morph (@morphs){
-#          print "Looking at morph $morph\n";
           my ($token, $pos) = $morph =~ m/(.+)\/(.+)/;
-#          print "Token is $token and pos is $pos\n";
           $expanded_lex .= " ($pos $token)";
         }
         $expanded_lex = substr $expanded_lex, 1;
-#        print "Replacing $lex with $expanded_lex\n";
         $tree =~ s/\Q$lex\E/$expanded_lex/e;
       }
       
