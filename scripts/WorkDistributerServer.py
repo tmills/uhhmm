5d927f24ecc3ee25db58db3b95636e3de41c7d7e
Tim Miller <timothy.miller@childrens.harvard.edu>
2017-02-09 07:05:26 -0500

    Fixes Issue 51. Workaround if socket API returns a host ip of 127. to connect outside and grab the IP address.

--- scripts/WorkDistributerServer.py
+++ scripts/WorkDistributerServer.py
@@ -269,7 +269,7 @@ class WorkDistributerServer():
     def __init__(self, sent_list, working_dir):
 
         ## Set up job distribution servers:
-        self.host = socket.gethostbyname(socket.gethostname())
+        self.host = get_local_ip()
         self.sent_list = sent_list
 
         context = zmq.Context()
@@ -357,4 +357,11 @@ class WorkDistributerServer():
         
     def get_parses(self):
         return self.sink.get_parses()
+
+## This function was required because of some funkiness on ubuntu systems where reverse dns lookup was returning a loopback ip
+## This will try the easy way and if it returns something with 127. will make an outside connectino to known DNS (8.8.8.8) and
+## grab the external IP from that socket.
+## Solution from here: http://stackoverflow.com/a/1267524
+def get_local_ip():
+    return [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
 
