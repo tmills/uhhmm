722bfc96e37fdb293ab7132584e4935df0e2baf2
jin <jin.544@osu.edu>
2017-03-19 16:26:49 -0400

    debugging

--- scripts/uhhmm.py
+++ scripts/uhhmm.py
@@ -305,6 +305,7 @@ def sample_beam(ev_seqs, params, report_function, checkpoint_function, working_d
             if parse.success:
                 try:
                     logging.info('The state sequence is ' + ' '.join([str(indexer.getStateIndex(x.j, x.a, x.b, x.f, x.g)) for x in parse.state_list]))
+                    logging.info(' '.join([x.str() for x in parse.state_list]))
                     increment_counts(parse.state_list, ev_seqs[ parse.index ], models)
 
                     # logging.info('Good parse:')
