6e4208698de898747af7aaca9dde10a1007031bb
jin <jin.544@osu.edu>
2017-04-07 17:42:34 -0400

    minor fix

--- gpusrc/HmmSampler.cu
+++ gpusrc/HmmSampler.cu
@@ -391,8 +391,11 @@ std::vector<float> HmmSampler::forward_pass(std::vector<std::vector<int> > sents
                 continue;
             } else if (sents[sent_ind].size() == ind){
                 int EOS = p_indexer -> get_EOS();
+                cout << EOS << endl;
                 array2d<float, device_memory>::column_view final_dyn_col = dyn_prog[ind - 1]->column(sent_ind);
+                print(cusp::blas::asum(final_dyn_col))
                 get_row(pi->get_view(), EOS, *trans_slice, pos_full_array, g_max, b_max);
+                print(cusp::blas::asum(*trans_slice))
                 float final_normalizer = cusp::blas::dot(*trans_slice, final_dyn_col);
                 log_probs[sent_ind] += log10f(final_normalizer);
                 continue;
