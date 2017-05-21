annealing with normalization - fixes

scripts/uhhmm.py
@@ -1365,8 +1365,8 @@ def normalize(matrix):
     matrix = matrix ** 10
     sums = np.sum(matrix, axis=-1, keepdims=True)
     sums = np.repeat(sums, matrix.shape[-1], axis=-1)
-    print(matrix)
-    print(sums)
+    print(matrix, matrix.shape)
+    print(sums, sums.shape)
     matrix /= sums
     matrix = np.nan_to_num(matrix)
     assert np.sum(matrix) == np.cumprod(matrix.shape)[-1], "{}, {}".format(np.sum(matrix), np.cumprod(matrix.shape)[-1])