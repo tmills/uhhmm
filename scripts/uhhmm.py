c4932dee10d9804478a205d5a884ef039dd16792
jin <jin.544@osu.edu>
2017-05-21 14:25:41 -0400

    annealing with normalization - fixes

--- scripts/uhhmm.py
+++ scripts/uhhmm.py
@@ -1360,8 +1360,9 @@ def unreanneal(models, ac_coeff=1, next_ac_coeff=1):
     return models
 
 def normalize(matrix):
-    assert len(matrix.shape) == 2, "shape of the normalizing matrix {} is not 2!".format(str(matrix.shape))
-    sums = np.sum(matrix, axis=1, keepdims=True)
-    sums = np.repeat(sums, matrix.shape[1], axis=1)
+    # assert len(matrix.shape) == 2, "shape of the normalizing matrix {} is not 2!".format(str(matrix.shape))
+    sums = np.sum(matrix, axis=-1, keepdims=True)
+    sums = np.repeat(sums, matrix.shape[-1], axis=-1)
     matrix /= sums
+    assert np.sum(matrix) == np.cumprod(matrix.shape)[-1], "{}, {}".format(np.sum(matrix), np.cumprod(matrix.shape)[-1])
     return matrix
