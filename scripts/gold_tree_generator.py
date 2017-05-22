0885e018d91543fccb823f2b6f8182076aa48db4
jin <jin.544@osu.edu>
2017-05-22 00:35:40 -0400

    add gold tree gen - fixes

--- scripts/gold_tree_generator.py
+++ scripts/gold_tree_generator.py
@@ -6,4 +6,4 @@ for line in sys.stdin:
         sys.stdout.write('(5 (4 (1 ' + line[0] + ') (2 ' + line[1] + ')) (3 ' + line[2] + '))' + '\n')
     elif len(line) == 7:
         sys.stdout.write('(5 (4 (1 ' + line[0] + ') (2 ' + line[1] + ')) (3 (3 (3 ' + line[2] +') (4 (1 '+ line[3] +
-                         ') (2 '+line[4] + '))) (4 (1' + line[5] + ') (2 ' + line[6] + '))))' + '\n')
+                         ') (2 '+line[4] + '))) (4 (1 ' + line[5] + ') (2 ' + line[6] + '))))' + '\n')
