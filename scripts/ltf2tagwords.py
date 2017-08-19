f9d4dd3525761448f749509810f5b6a640664117
jin <jin.544@osu.edu>
2017-08-18 22:16:55 -0400

    il5 il6 indices

--- scripts/ltf2tagwords.py
+++ scripts/ltf2tagwords.py
@@ -3,6 +3,9 @@ import glob
 import sys
 
 def main(args):
+    if len(args) == 3:
+        outputfile = open(args[1], 'w')
+        outindexfile = open(args[2], 'w')
     if sys.version_info[0] != 3:
         print("This script requires Python 3")
         exit()
@@ -13,21 +16,28 @@ def main(args):
         try:
             tree = ET.parse(ltf_file)
             root = tree.getroot()
-            for seg in root.iter('SEG'):
-                cur_sent = list()
-                for token in seg.iter('TOKEN'):
-                    tag = token.attrib.get('pos')
-                    if tag == None:
-                        tag = 'UNK'
-                        #cur_sent = None
-                        #break
-                
-                    tag = tag.replace('/', '-slash-')
-                    word = token.text
-                    cur_sent.append("%s/%s" % (tag, word))
-                
-                if cur_sent != None:
-                    print(' '.join(cur_sent))
+            for doc in root.iter('DOC'):
+                cur_doc = doc.attrib.get('id')
+                for seg in root.iter('SEG'):
+                    cur_seg = seg.attrib.get('id')
+                    cur_sent = list()
+                    for token in seg.iter('TOKEN'):
+                        tag = token.attrib.get('pos')
+                        if tag == None:
+                            tag = 'UNK'
+                            #cur_sent = None
+                            #break
+
+                        tag = tag.replace('/', '-slash-')
+                        word = token.text
+                        cur_sent.append("%s/%s" % (tag, word))
+
+                    if cur_sent != None:
+                        if len(args) == 3:
+                            print(' '.join(cur_sent), file=outputfile)
+                            print(' '.join([ltf_file, cur_doc, cur_seg]), file=outindexfile)
+                        else:
+                            print(' '.join(cur_sent))
         except:
             sys.stderr.write("Error parsing file %s\n" % ltf_file)
 
--- utils/il5_il6_process.py
+++ utils/il5_il6_process.py
@@ -1,8 +1,12 @@
 import sys
+# take the words file as input, not tagwords
 filename = sys.argv[1]
+indexfilename = sys.argv[2]
 
-with open(filename, encoding='utf8') as i, open(filename+'.clean.txt', 'w', encoding='utf8') as o:
-    for line in i:
+with open(filename, encoding='utf8') as i, open(filename+'.clean.txt', 'w', encoding='utf8') as o, open(indexfilename, encoding='utf8') as index_i,\
+    open(indexfilename+'.clean.txt', 'w') as index_w:
+    all_indices = index_i.readlines()
+    for index, line in enumerate(i):
         segs = line.strip().split(' ')
         if len(segs) == 1 or len(segs) > 40:
             continue
@@ -13,3 +17,4 @@ with open(filename, encoding='utf8') as i, open(filename+'.clean.txt', 'w', enco
             else:
                 good_segs.append(seg.lower())
         print(' '.join(good_segs), file=o)
+        print(all_indices[index].strip(), file=index_w)
