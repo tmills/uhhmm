d8b5e546f4163631ae85b1a97fc8994cc748ecb6
jin <jin.544@osu.edu>
2017-10-18 16:15:13 -0400

    fixes

--- utils/add_x_nonterms.py
+++ utils/add_x_nonterms.py
@@ -14,4 +14,4 @@ with open(fn) as i, open(fn+'.proper', 'w') as o:
             else:
                 line = line.replace(word, '(' + word + ')')
         line = re.sub('\(\s*', '(X ', line)
-        print(line, file=o)
+        o.write(line, file=o)
